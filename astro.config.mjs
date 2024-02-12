5421b4832fc8e61ca3ebd474761d5549efd67657
Nick Taylor <nick@nickyt.co>
2024-02-12 08:35:46 -0500

    chore: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind({
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: netlify()
+});
