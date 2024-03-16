7bfb8a4700784c21c6532091211c0008b180e0a6
Nick Taylor <nick@nickyt.co>
2024-03-16 12:03:14 -0400

    chore: fixed types for Astro.locals.netlify

--- types/astro-extended.d.ts
+++ types/astro-extended.d.ts
@@ -1,12 +1,14 @@
 declare namespace App {
   export interface Locals {
     netlify: {
-      geo: {
-        timezone: string
-        city: string
-        country: string
-        latitude: number
-        longitude: number
+      context: {
+        geo: {
+          timezone: string
+          city: string
+          country: string
+          latitude: number
+          longitude: number
+        }
       }
     }
   }
