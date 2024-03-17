4a90da595499ea72d6ecd06239869d0a3020b0fa
Nick Taylor <nick@nickyt.co>
2024-03-16 22:40:03 -0400

    chore: converted heading anchor string to a component

--- src/utils/schedule-utils.ts
+++ src/utils/schedule-utils.ts
@@ -1,15 +1,5 @@
 import type { StreamGuestInfo } from "../components/StreamSchedule.astro";
 
-export function buildHeadingAnchor(headingId: string) {
-  return `
-  <a href="#${headingId}" class="heading-permalink">
-        <span class="visually-hidden"> permalink</span>
-        <svg fill="currentColor" aria-hidden="true" focusable="false" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path d="M9.199 13.599a5.99 5.99 0 0 0 3.949 2.345 5.987 5.987 0 0 0 5.105-1.702l2.995-2.994a5.992 5.992 0 0 0 1.695-4.285 5.976 5.976 0 0 0-1.831-4.211 5.99 5.99 0 0 0-6.431-1.242 6.003 6.003 0 0 0-1.905 1.24l-1.731 1.721a.999.999 0 1 0 1.41 1.418l1.709-1.699a3.985 3.985 0 0 1 2.761-1.123 3.975 3.975 0 0 1 2.799 1.122 3.997 3.997 0 0 1 .111 5.644l-3.005 3.006a3.982 3.982 0 0 1-3.395 1.126 3.987 3.987 0 0 1-2.632-1.563A1 1 0 0 0 9.201 13.6zm5.602-3.198a5.99 5.99 0 0 0-3.949-2.345 5.987 5.987 0 0 0-5.105 1.702l-2.995 2.994a5.992 5.992 0 0 0-1.695 4.285 5.976 5.976 0 0 0 1.831 4.211 5.99 5.99 0 0 0 6.431 1.242 6.003 6.003 0 0 0 1.905-1.24l1.723-1.723a.999.999 0 1 0-1.414-1.414L9.836 19.81a3.985 3.985 0 0 1-2.761 1.123 3.975 3.975 0 0 1-2.799-1.122 3.997 3.997 0 0 1-.111-5.644l3.005-3.006a3.982 3.982 0 0 1 3.395-1.126 3.987 3.987 0 0 1 2.632 1.563 1 1 0 0 0 1.602-1.198z"></path>
-        </svg></a>
-  `;
-}
-
 export function getHeadingId(name: string, dateTime: string) {
   const [date] = dateTime.split("T");
   return `${date}-${encodeURIComponent(name.replace(/\s+/, "-"))}`.toLowerCase();
@@ -34,70 +24,6 @@ export function getLocalizedDate({
   };
 
   return new Date(date).toLocaleString(locale, options);
-}
-
-export function getScheduleMarkup({
-  schedule,
-  locale,
-  timezone,
-}: {
-  schedule: StreamGuestInfo[];
-  locale: string;
-  timezone: string;
-}) {
-  const scheduleMarkup = schedule
-    .map(
-      (
-        {
-          date,
-          streamTitle,
-          streamDescription,
-          youtubeStreamLink,
-          name,
-          title,
-          twitter,
-          website,
-          twitch,
-          youtube,
-          github,
-        },
-      ) => {
-        const guestDate = getLocalizedDate({
-          date,
-          locale,
-          timezone,
-          showTime: true,
-        });
-        const headingId = getHeadingId(name, date);
-
-        return `
-    <li class="post-list__item">
-      <h2 id="${headingId}">${streamTitle} ${buildHeadingAnchor(headingId)}</h2>
-      <time datetime="${date}">${guestDate}</time>
-      <div>
-        ${github === "nickytonline" ? "" : `<div>Guest: ${name}${title ? `, ${title}` : ""}</div>`}
-        <nav class="nav" aria-label="Links for live stream guest ${name}">
-          <ul>
-          ${buildWebsiteLink({ name, website })}
-          ${buildGithubLink({ name, github })}
-          ${buildTwitterLink({ name, twitter })}
-          ${buildTwitchLink({ name, twitch })}
-          ${buildYoutubeLink({ name, youtube })}
-          </ul>
-        </nav>
-        ${youtubeStreamLink ? getStreamLinks(youtubeStreamLink) : ""}
-        ${streamDescription
-            ? `<p class="gap-top-300">${streamDescription}</p>`
-            : ``
-          }
-      </div>
-    </li>
-  `;
-      }
-    )
-    .join("");
-
-  return `<ol class="[ post-list__items ] [ sf-flow ] [ pad-top-300 ]">${scheduleMarkup}</ol>`;
 }
 
 export function getLatestGuestMarkup({
