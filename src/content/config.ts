b3f6ff466a3e4b8035ade86d5da6dac27f7ad6d7
Nick Taylor <nick@nickyt.co>
2025-04-27 10:30:57 -0400

    created blog pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,6 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.coerce.date(), // Ensures ISO date
-    slug: z.string(),
     description: z.string().optional(),
     tags: z.array(z.string()).optional(),
     cover: z.string().optional(),
