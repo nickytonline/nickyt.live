6ccb16c8f384ca2004595cfd9584c0cbb21a1160
Nick Taylor <nick@nickyt.co>
2026-01-10 22:09:27 -0500

    chore: this is now a redirect site

--- src/utils/domUtils.ts
+++
@@ -1,7 +0,0 @@
-export const createCloneElement = (text: string): HTMLSpanElement => {
-  const clone = document.createElement("span");
-  clone.textContent = text;
-  clone.style.position = "absolute";
-  clone.style.opacity = "0";
-  return clone;
-};
--- types/astro-extended.d.ts
+++
@@ -1,5 +0,0 @@
-type NetlifyLocals = import("@astrojs/netlify").NetlifyLocals;
-
-declare namespace App {
-  interface Locals extends NetlifyLocals {}
-}
